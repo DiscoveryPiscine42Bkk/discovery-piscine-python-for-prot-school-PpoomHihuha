1e09aa5398f8ec59b7336300d9699756f8eab75a
PpoomHihuha <rchurdun@student.42bangkok.com>
2025-03-13 09:41:17 +0700

    Add files via upload

---
+++ cell02/ex2.0/ex00/iszero.py
@@ -0,0 +1,9 @@
+  number = float(input("Please enter a number: 30").strip())
+
+   
+    if number == 0:
+        print("This number is equal to zero.")
+    else:
+        print("This number is different from zero.")
+except ValueError:
+    print("Invalid input! Please enter a valid number.")
